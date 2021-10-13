a397b21012cd2657b42a9d6c15b042e2ea4a2a2d
chelseatwimasi <89853022+chelseatwimasi@users.noreply.github.com>
2021-10-13 12:27:16 -0400

    Today's work

--- src/components/descriptions.js
+++ src/components/descriptions.js
@@ -6,30 +6,24 @@ function Descriptions() {
     <div className="container-fluid Descriptions">
       <div className="row">
         <div className="col">
-          <h5 className="title-text">Real Cats</h5>
+          <h5 className="title-text">Creepy Cats</h5>
           <p className="sub-text">
-            Contrary to popular belief, cats aren't loners. At furbook, we know
-            cats need friendship, affection, and comradery. It is our core
-            belief that beneath every cat's hard, angry, exterior there is a pet
-            who jus wants to play and cuddle with a friend
+            Cats are very creepy and disturbing. Everytime you look at one,
+            they're ready to attack you with not type of hesitation. Try not to
+            be near one, for if they get their paws on you and scratch you, you
+            may not like it.
           </p>
         </div>
         <div className="col">
-          <h5 className="title-text">Real Dogs</h5>
+          <h5 className="title-text">Dirty Disturbing Dogs</h5>
           <p className="sub-text">
-            Dogs are man's best friend. But dogs can also be dog's best friend.
-            Maybe even bester than man's best friend. On furbook, you can find
-            millions of other dogs who can be a loyal companion, trusted
-            confidant, and reliable wrestling partner
-          </p>
-        </div>
-        <div className="col">
-          <h5 className="title-text">Real Friends</h5>
-          <p className="sub-text">
-            s Who says cats and dogs can't get along? Here at Furbook, we
-            believe all pets can be best friends. Our furbook users believe it
-            doesn't matter if you are a cat, dog, rabbit, or ferret; underneath
-            that fur we are all cute, wonderful pets.{" "}
+            Dogs are even more annoying than cats. They're always running around
+            and trying to chase after you when you just could be simply minding
+            your own business and getting lost in your own wrold. Everytime you
+            walk by them, you have to be cautious of how you approach them, for
+            they will see you as a threat and just attack you. They also don't
+            know how to clean after themselves. They poop everywhere and
+            anywhere and think that it is fine.
           </p>
         </div>
       </div>
